72b5f056057ca51db8e0f7bfbacb642e03eeb931
Abhishek Kumar <abhi.kr.2100@gmail.com>
2020-12-10 17:44:39 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="SVBot",
+    version="0.1.0",
+
+    description="A modular and extensible Discord bot.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    author="Abhishek Kumar",
+    author_email="abhi.kr.2100@gmail.com",
+
+    scripts=['bot.py'],
+    packages=find_packages()
+)
