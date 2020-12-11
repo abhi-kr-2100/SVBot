2058cc4c0cceb019c3634c11d3287194a455d293
Abhishek Kumar <abhi.kr.2100@gmail.com>
2020-12-11 20:37:22 +0530

    Implement !qt to work when no tag or non-existent tag is passed

--- cogs/Quotes.py
+++ cogs/Quotes.py
@@ -26,24 +26,44 @@ class Quotes(commands.Cog):
         name='qt',
         help="Display a quote related to the given tag."
     )
-    async def quote(self, ctx, tag):
+    async def quote(self, ctx, tag=None):
         """Reply with a relevent quote given a tag."""
 
-        tag = tag.lower()
+        if tag is None:
+            quote = choice(
+                self.c.execute(
+                    'SELECT QuoteText FROM quotes'
+                ).fetchall()
+            )[0]
+        else:
+            tag = tag.lower()
 
-        tag_id = self.c.execute(
-            'SELECT TagId FROM tags WHERE TagName=?', (tag,)
-        ).fetchone()
+            tag_id = self.c.execute(
+                'SELECT TagId FROM tags WHERE TagName=?', (tag,)
+            ).fetchone()
 
-        quote_id = choice(
-            self.c.execute(
-                'SELECT QuoteId FROM links WHERE TagId=?', tag_id
-            ).fetchall()
-        )
+            if tag_id is None:
+                await reply(
+                    ctx,
+                    f"No quotes tagged {tag} were found. "
+                    "Here's a random quote instead:"
+                )
 
-        quote = self.c.execute(
-            'SELECT QuoteText FROM quotes WHERE QuoteId=?', quote_id
-        ).fetchone()[0]
+                quote = choice(
+                    self.c.execute(
+                        'SELECT QuoteText FROM quotes'
+                    ).fetchall()
+                )[0]
+            else:
+                quote_id = choice(
+                    self.c.execute(
+                        'SELECT QuoteId FROM links WHERE TagId=?', tag_id
+                    ).fetchall()
+                )
+
+                quote = self.c.execute(
+                    'SELECT QuoteText FROM quotes WHERE QuoteId=?', quote_id
+                ).fetchone()[0]
 
         await reply(ctx, quote)
 
