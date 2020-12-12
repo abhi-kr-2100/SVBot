f7ad34f883bb4a29566fddc9b60da6a12867b381
Abhishek Kumar <abhi.kr.2100@gmail.com>
2020-12-12 23:57:37 +0530

    Add classes related to players

---
+++ cogs/PokerAux/Player.py
@@ -0,0 +1,28 @@
+"""
+Auxiliary module for the Poker Cog. This module defines classes related to
+poker players.
+"""
+
+
+from enum import Enum, auto, unique
+
+
+@unique
+class PlayerStatus(Enum):
+    """Player's status based on sitting position."""
+
+    small_blind     = auto()
+    big_blind       = auto()
+    dealer          = auto()
+    normal          = auto()
+    small_dealer    = auto()    # when there are only two players
+
+
+class Player:
+    """A poker player."""
+
+    def __init__(self, member, chips, status=PlayerStatus.normal):
+        self.member = member
+        self.chips = chips
+        self.status = status
+        self.turn_pending = False
