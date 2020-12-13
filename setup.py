c1d93f1e4e20d48a7f7f83314c88c5b818997be8
Abhishek Kumar <abhi.kr.2100@gmail.com>
2020-12-13 12:33:53 +0530

    Version 0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SVBot",
-    version="0.3.0",
+    version="0.4.0",
 
     description="A modular and extensible Discord bot.",
     long_description=open('README.md').read(),
