59b001856c95f3ff1f6631eddf0a527a305424d0
Abhishek Kumar <abhi.kr.2100@gmail.com>
2020-12-15 14:41:23 +0530

    Add !boris command

---
+++ cogs/Boris.py
@@ -0,0 +1,27 @@
+"""Display the :boris: emoji."""
+
+
+from random import random
+
+from discord.ext import commands, tasks
+
+
+class Boris(commands.Cog):
+    """Display the :boris: emoji when called."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.ctx = None
+
+    @commands.command(
+        name='boris',
+        help='Display the Boris emoji.'
+    )
+    async def boris(self, ctx):
+        """Display :Boris: at arbitrary times."""
+
+        await ctx.send("<:Boris:756477965784842261>")
+
+
+def setup(bot):
+    bot.add_cog(Boris(bot))
