a90f36e0bf6ee6710c3b3970cfd73f9800e4a130
Abhishek Kumar <abhi.kr.2100@gmail.com>
2020-12-22 20:35:59 +0530

    Collect named constants in a new module

---
+++ cogs/PokerAux/Constants.py
@@ -0,0 +1,11 @@
+"""Named constants used by Poker.py and related modules."""
+
+
+# the name of the channel in which poker can be played
+CHANNEL_NAME = 'poker'
+
+# the number of chips each player has at the start of the game
+STARTING_CHIPS = 1000
+
+# the small blind bet amount; the big blind bet amount is always twice this
+SM_BLIND_BET = 10
