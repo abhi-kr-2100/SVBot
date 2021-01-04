09a9265a67fbd571882d5fe65973998f6ffe7094
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-01-04 21:14:16 +0530

    Notify me when an important person is typing

--- cogs/NotifyMeOnly.py
+++ cogs/NotifyMeOnly.py
@@ -1,6 +1,12 @@
 """DM me (and only me!) when a given user sends a message."""
 
 
+from typing import Union
+from datetime import datetime
+
+from discord.abc import Messageable
+from discord import Member, User
+
 from discord.ext import commands
 import discord
 
@@ -19,6 +25,15 @@ class NotifyMeOnly(commands.Cog):
 
         if msg.author.display_name == 'theStudyVibes':
             await self.me.send("Alert! Alert!")
+
+    @commands.Cog.listener()
+    async def on_typing(self, channel: Messageable, user: Union[User, Member],
+                        when: datetime):
+        if self.me is None:
+            self.me = await self.bot.fetch_user(711994085480726639)
+
+        if user.display_name == 'theStudyVibes':
+            await self.me.send("Alert!")
 
 
 def setup(bot: commands.Bot):
