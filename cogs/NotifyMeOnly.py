f8e29e4dbec127ba958a0fd904057119faa0bff6
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-01-04 21:15:10 +0530

    Refactor

--- cogs/NotifyMeOnly.py
+++ cogs/NotifyMeOnly.py
@@ -5,7 +5,7 @@ from typing import Union
 from datetime import datetime
 
 from discord.abc import Messageable
-from discord import Member, User
+from discord import Member, User, Message
 
 from discord.ext import commands
 import discord
@@ -16,12 +16,13 @@ class NotifyMeOnly(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.my_id = 711994085480726639
         self.me = None
 
     @commands.Cog.listener()
-    async def on_message(self, msg: discord.Message):
+    async def on_message(self, msg: Message):
         if self.me is None:
-            self.me = await self.bot.fetch_user(711994085480726639)
+            self.me = await self.bot.fetch_user(self.my_id)
 
         if msg.author.display_name == 'theStudyVibes':
             await self.me.send("Alert! Alert!")
@@ -30,7 +31,7 @@ class NotifyMeOnly(commands.Cog):
     async def on_typing(self, channel: Messageable, user: Union[User, Member],
                         when: datetime):
         if self.me is None:
-            self.me = await self.bot.fetch_user(711994085480726639)
+            self.me = await self.bot.fetch_user(self.my_id)
 
         if user.display_name == 'theStudyVibes':
             await self.me.send("Alert!")
